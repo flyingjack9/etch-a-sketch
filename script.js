41a5d7306f5d0a674cb0d9b8ddb0dbd736051fbc
flyingjack9 <andrechenail14@gmail.com>
2022-04-12 15:25:55 -0400

    Completed a mockup of what the grid should look like and behave in accordance to its size

---
+++ script.js
@@ -0,0 +1,15 @@
+const grid = document.querySelector("#sketch-panel");
+
+let gridSize = 16;
+
+function test() {
+    alert("ok");
+}
+
+// function createGrid() {
+//     for (let i = 0; i < gridSize; i++) {
+//         const row = document.createElement("div");
+//         row.classList.add("")
+
+//     }
+// }
