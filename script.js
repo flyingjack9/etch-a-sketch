4849cfd5f584d2fa17288e08ac079b147b23dcce
flyingjack9 <andrechenail14@gmail.com>
2022-04-12 16:09:09 -0400

    Tested out various ways to create the grid and its properties according to the user, I think I may have the general idea now in Javascript and CSS

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const grid = document.querySelector("#sketch-panel");
+const stylesheet = document.styleSheets[0];
 
 let gridSize = 16;
+let gridSquareMeasure;
 
 function test() {
-    alert("ok");
+    gridSize = prompt("Please enter a the size of the grid : ");
+    gridSquareMeasure = 100 / gridSize;
+    console.log(gridSquareMeasure);
+    stylesheet.insertRule(`.grid-square { width: ${gridSquareMeasure}%; padding-bottom: ${gridSquareMeasure}%;`);
+    createGrid();
 }
 
-// function createGrid() {
-//     for (let i = 0; i < gridSize; i++) {
-//         const row = document.createElement("div");
-//         row.classList.add("")
+function createGrid() {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
 
-//     }
-// }
+        for (let j = 0; j < gridSize; j++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            row.appendChild(gridSquare);
+        }
+        grid.appendChild(row);
+
+    }
+}
