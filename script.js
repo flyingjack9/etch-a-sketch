e81a1a85c8c48c2581eda3f295edf6c77dbb09e4
flyingjack9 <andrechenail14@gmail.com>
2022-04-13 15:48:12 -0400

    Completed most of the Javascript for this project, will do more changes to change a few functionalities before its done. Also added more html for additional buttons

--- script.js
+++ script.js
@@ -1,13 +1,67 @@
-const grid = document.querySelector("#sketch-panel");
+// Variables and constants
+const resetBtn = document.getElementById("reset");
+const eraserBtn = document.getElementById("eraser");
+const blackBtn = document.getElementById("black");
+const rainbowBtn = document.getElementById("rainbow");
+const randomBtn = document.getElementById("random");
+const changeSizeBtn = document.getElementById("changeSize");
+
+const grid = document.getElementById("sketch-panel");
 const stylesheet = document.styleSheets[0];
 
 let gridSize = 16;
 let gridSquareMeasure;
+let currentPenColor = "black";
+let randomColor;
 
-function test() {
-    gridSize = prompt("Please enter a the size of the grid : ");
-    gridSquareMeasure = 100 / gridSize;
-    console.log(gridSquareMeasure);
+// Events listeners
+grid.addEventListener("mouseover", function(event) {
+    switch (currentPenColor) {
+        case "eraser":
+            event.target.style.backgroundColor = "greenyellow";
+            break;
+
+        case "black":
+            event.target.style.backgroundColor = "black";
+            break;
+
+        case "rainbow":
+            event.target.style.backgroundColor = generateRandomRGB();
+            break;
+            
+        case "random":
+            event.target.style.backgroundColor = randomColor;
+            break;   
+    }
+})
+
+resetBtn.addEventListener("click", resetGrid);
+
+eraserBtn.addEventListener("click", () => {
+    currentPenColor = "eraser";
+});
+
+blackBtn.addEventListener("click", () => {
+    currentPenColor = "black";
+});
+
+rainbowBtn.addEventListener("click", () => {
+    currentPenColor = "rainbow";
+});
+
+randomBtn.addEventListener("click", () => {
+    randomColor = generateRandomRGB();
+    currentPenColor = "random";
+});
+
+changeSizeBtn.addEventListener("click", () => {
+    location.reload();
+})
+
+// Functions
+function initiateSketchBoard() {
+    getGridSize();
+    calculateGridRatio();
     stylesheet.insertRule(`.grid-square { width: ${gridSquareMeasure}%; padding-bottom: ${gridSquareMeasure}%;`);
     createGrid();
 }
@@ -24,5 +78,34 @@ function createGrid() {
         }
         grid.appendChild(row);
 
+    }
+}
+
+function getGridSize() {
+    gridSize = prompt("Please enter a number between 1 - 100 for the size of the grid : ");
+    if (gridSize <= 0 || gridSize > 100) {
+        alert("Invalid entry, please try again!");
+        getGridSize();
+    }
+}
+
+function calculateGridRatio() {
+    gridSquareMeasure = 100 / gridSize;
+}
+
+function generateRandomRGB() {
+    let letters = "0123456789ABCDEF".split("");
+    let randomColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+        randomColor += letters[Math.round(Math.random() * 15)];
+    }
+    return randomColor;
+}
+
+function resetGrid() {
+    let gridSquares = document.getElementsByClassName("grid-square");
+    for (let i = 0; i < gridSquares.length; i++) {
+        gridSquares[i].style.backgroundColor = "greenyellow";
     }
 }
